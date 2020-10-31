cbf50d56f57208826358b4761881e4fcc82edd15
Aaditya Chandrasekhar <aaditya.c@phiar.net>
2020-10-30 23:18:40 -0700

    Add file server over http interface for yautja interfacing.
    A new namespace called `yautja` is introduced that provides the
    following file server functions:
    
    1. create-directory
    2. delete
    3. read-directory
    4. read-file
    5. rename (to be implemented)
    6. stat (to be implemented)
    7. write-file (to be implemented)
    
    4/7 server functions are implemented as they are simple [command + uri]
    pairings.
    
    FS API reference for yautja/vscode that this patch was implemented for
    is located at https://github.com/microsoft/vscode-web-playground/blob/main/src/memfs.ts.

--- Sources/baikonur/main.swift
+++ Sources/baikonur/main.swift
@@ -5,8 +5,10 @@ import Vapor
 internal typealias Option = ArgumentParser.Option
 
 internal struct ServerSessionState: ParsableCommand {
-    static private var _serverPort = 0
+    static private var _serverPort = 4000
+    static private var _yautjaConfig = YautjaServiceConfiguration()
     static private var serialQueue = DispatchQueue(label: "ServerSessionState.serialQueue")
+
     static fileprivate var serverPort: Int {
         get {
             return self.serialQueue.sync { self._serverPort }
@@ -17,15 +19,30 @@ internal struct ServerSessionState: ParsableCommand {
         }
     }
 
+    static fileprivate var yautjaConfig: YautjaServiceConfiguration {
+        get {
+            return self.serialQueue.sync { self._yautjaConfig }
+        }
+
+        set {
+            self.serialQueue.async { self._yautjaConfig = newValue }
+        }
+    }
+
     @Option(name: .shortAndLong, help: "Enter port number")
     internal var port: Int?
 
+    @Option(name: .shortAndLong, help: "File system extension root")
+    internal var fsRoot: String?
+
     mutating func run() throws {
-        guard let _port = port else {
-            preconditionFailure("Got nil for port.")
+        if let port = self.port {
+            ServerSessionState.serverPort = port
         }
 
-        ServerSessionState.serverPort = _port
+        if let fsRoot = self.fsRoot {
+            ServerSessionState.yautjaConfig.fsRoot = fsRoot
+        }
     }
 }
 
@@ -39,8 +56,15 @@ public func configure(_ app: Application) throws {
 }
 
 public func routes(_ app: Application) throws {
-    app.get { req in
-        return "Hello, world!"
+    app.get("yautja", ":command") { req -> String in
+        guard let _command = req.parameters.get("command"),
+              let command = YautjaServiceCommand(rawValue: _command),
+              let serviceArguments = try? req.query.decode(YautjaServiceArguments.self) else {
+            return ""
+        }
+
+        return command.perform(config: ServerSessionState.yautjaConfig,
+                               arguments: serviceArguments)
     }
 }
 
@@ -51,6 +75,26 @@ try LoggingSystem.bootstrap(from: &env)
 let app = Application(env)
 
 defer { app.shutdown() }
+
+let corsConfiguration = CORSMiddleware.Configuration(allowedOrigin: .all,
+                                                     allowedMethods: [
+                                                         .GET,
+                                                         .POST,
+                                                     ],
+                                                     allowedHeaders: [
+                                                         .accept,
+                                                         .accessControlAllowOrigin,
+                                                         .authorization,
+                                                         .contentType,
+                                                         .origin,
+                                                         .userAgent,
+                                                         .xRequestedWith,
+                                                     ])
+let corsMiddleware = CORSMiddleware(configuration: corsConfiguration)
+var middleware = Middlewares()
+
+middleware.use(corsMiddleware)
+app.middleware = middleware
 
 ServerSessionState.main()
 try configure(app)
